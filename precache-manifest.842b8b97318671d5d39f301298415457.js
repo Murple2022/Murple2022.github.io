09937a987ee3f73791e8666959ad694eb2934654
Murple2022 <100107407+Murple2022@users.noreply.github.com>
2022-02-21 12:25:27 +0900

    Add files via upload

---
+++ precache-manifest.842b8b97318671d5d39f301298415457.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7e5904fc3da6f2804d4",
+    "url": "/css/app.a17a0c97.css"
+  },
+  {
+    "revision": "d7e5904fc3da6f2804d4",
+    "url": "/js/app.968fe53a.js"
+  },
+  {
+    "revision": "9109de386a3c03e9264a",
+    "url": "/js/chunk-vendors.60155b6e.js"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "669a27daa9fb02e006953012917e9d31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f5bac26c3d4cefd26ace485eb798e74",
+    "url": "/img/brand/Murple_Logo_Black.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "b2713725f68dcae3e91e0fa2d68fddbb",
+    "url": "/img/brand/Murple_Logo_White.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "441e15f52d2ce8848632cb2c064e9682",
+    "url": "/img/theme/artificial-intelligence-gfe2f59dab_1280.jpg"
+  },
+  {
+    "revision": "eb307627d8929a011c02cdf9b3de6864",
+    "url": "/img/theme/person-gb2851e263_1280.png"
+  },
+  {
+    "revision": "83b89bc9a67f8e973d673217cdf04a1c",
+    "url": "/img/theme/recording-studio-g807751001_640.jpg"
+  }
+];
