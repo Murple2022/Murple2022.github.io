1bdf537609e2f814d58b4e15d16aeb21984ba6bc
jaegeon <murple2022@gmail.com>
2022-02-24 18:36:13 +0900

    v0.0.2

--- precache-manifest.d555f00e247412d49483ad3d9cadb83f.js
+++ precache-manifest.d555f00e247412d49483ad3d9cadb83f.js
@@ -1,43 +1,43 @@
 self.__precacheManifest = [
   {
-    "revision": "0b9f24af21292ee75aec",
+    "revision": "aef46f51152b0e35b99a",
     "url": "/css/app.a17a0c97.css"
   },
   {
-    "revision": "0b9f24af21292ee75aec",
-    "url": "/js/app.5e4a22ff.js"
+    "revision": "aef46f51152b0e35b99a",
+    "url": "/js/app.c69a8617.js"
   },
   {
     "revision": "9109de386a3c03e9264a",
     "url": "/js/chunk-vendors.60155b6e.js"
   },
   {
-    "revision": "c1733565b32b585676302d4233c39da8",
-    "url": "/fonts/nucleo-icons.c1733565.eot"
-  },
-  {
     "revision": "426439788ec5ba772cdf94057f6f4659",
     "url": "/fonts/nucleo-icons.42643978.woff2"
-  },
-  {
-    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
-    "url": "/fonts/nucleo-icons.2569aaea.woff"
   },
   {
     "revision": "f82ec6ba2dc4181db2af35c499462840",
     "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
   },
   {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
     "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
     "url": "/img/nucleo-icons.0b8a30b1.svg"
   },
   {
-    "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
-  },
-  {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
     "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
   },
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
@@ -52,7 +52,7 @@ self.__precacheManifest = [
     "url": "/img/fontawesome-webfont.912ec66d.svg"
   },
   {
-    "revision": "0e90b07c982da94dab9ee228f93e7b1e",
+    "revision": "864742b0833ed90081cde69b099d8c3c",
     "url": "/index.html"
   },
   {
@@ -60,20 +60,20 @@ self.__precacheManifest = [
     "url": "/favicon.png"
   },
   {
-    "revision": "b2713725f68dcae3e91e0fa2d68fddbb",
-    "url": "/img/brand/Murple_Logo_White.png"
-  },
-  {
     "revision": "2f5bac26c3d4cefd26ace485eb798e74",
     "url": "/img/brand/Murple_Logo_Black.png"
   },
   {
-    "revision": "594b1ee1d95ada356eaad078e9217932",
-    "url": "/img/ill/ill-2.svg"
+    "revision": "b2713725f68dcae3e91e0fa2d68fddbb",
+    "url": "/img/brand/Murple_Logo_White.png"
   },
   {
     "revision": "eb307627d8929a011c02cdf9b3de6864",
     "url": "/img/theme/person-gb2851e263_1280.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/img/ill/ill-2.svg"
   },
   {
     "revision": "83b89bc9a67f8e973d673217cdf04a1c",
